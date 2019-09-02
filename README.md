381a5b08f02b0631e7ee30545deb3711f26a09b6
Matthew Vernacchia <mvernacc@mit.edu>
2019-09-02 15:57:45 -0400

    Add demo module (woowaa) and tests.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # learn-travis-and-pytest
 Practice using Travis CI and pytest
+
+Based on [this tutorial](http://luisquintanilla.me/2018/02/18/testing-deploying-python-projects-travisci/)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='learn-travis-and-pytest',
+    packages=['woowaa'],
+    version='0.0.1',
+    description='Test project to get acquainted with TravisCI',
+    url='https://github.com/mvernacc/learn-travis-and-pytest',    
+)
